9787e6ca53bf97d60e19856fc1d107155fa5ab11
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-03-21 13:31:50 +0100

    Implement the home component

--- Adminpanel/src/Home.jsx
+++ Adminpanel/src/Home.jsx
@@ -6,11 +6,13 @@ function Home() {
       <div className="main-title">
         <h3>DASHBOARD</h3>
       </div>
-      
+
       <div className="main-cards">
         <div className="card-inner">
           <h3>PRODUCTS</h3>
+          <></>
         </div>
+        <h1>300</h1>
       </div>
     </main>
   );
