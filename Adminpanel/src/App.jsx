9d0be07d1644faf56b1d2751a4fa60a42e6951da
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-03-21 12:34:00 +0100

    Added the component

--- Adminpanel/src/App.jsx
+++ Adminpanel/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import SideBar from './SideBar';
 function App() {
-
   return (
     <div className='grid-container'>
-
+      <Header/>
+      <SideBar/>
+      <Home/>
     </div>
   )
 }
--- Adminpanel/src/Sidebar.jsx
+++ Adminpanel/src/Sidebar.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { BsCart3 } from "react-icons/bs";
 
 function SideBar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <aside id="sidebar">
+      <div className="sidebar-title">
+        <div className="sidebar-brand">
+          <BsCart3 className="icon_header" /> SHOP
+        </div>
+        <span className="icon close_icon">X</span>
+      </div>
+
+
+      <ul className="sidebar-list">
+        <li  className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Dashboard
+            </a>
+        </li>
+        <li className="sidebar-list-item" >
+            <a href="">
+                <BsCart3 className="icon"/> Products
+            </a>
+        </li>
+        <li className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Categories
+            </a>
+        </li>
+        <li className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Customers
+            </a>
+        </li>
+        <li className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Inventory
+            </a>
+        </li>
+        <li className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Reports
+            </a>
+        </li>
+        <li className="sidebar-list-item">
+            <a href="">
+                <BsCart3 className="icon"/> Setting
+            </a>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
-export default SideBar
+export default SideBar;
