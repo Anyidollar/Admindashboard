19541bc4ac0e26cd704b6864ee6bb781abb638d5
Anyidollar <ifeanyionyekwere247@gmail.com>
2025-03-21 13:27:20 +0100

    Added products components

--- Adminpanel/src/Home.jsx
+++ Adminpanel/src/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
   return (
-    <div>
-    
-    </div>
-  )
+    <main className="main-container">
+      <div className="main-title">
+        <h3>DASHBOARD</h3>
+      </div>
+      
+      <div className="main-cards">
+        <div className="card-inner">
+          <h3>PRODUCTS</h3>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
--- Adminpanel/src/Sidebar.jsx
+++ Adminpanel/src/Sidebar.jsx
@@ -1,56 +1,58 @@
-import React from "react";
-import { BsCart3 } from "react-icons/bs";
+import React from 'react'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
 
-function SideBar() {
+function Sidebar() {
   return (
-    <aside id="sidebar">
-      <div className="sidebar-title">
-        <div className="sidebar-brand">
-          <BsCart3 className="icon_header" /> SHOP
+    <aside id="sidebar" >
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+                <BsCart3  className='icon_header'/> SHOP
+            </div>
+            <span className='icon close_icon'>X</span>
         </div>
-        <span className="icon close_icon">X</span>
-      </div>
 
-
-      <ul className="sidebar-list">
-        <li  className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Dashboard
-            </a>
-        </li>
-        <li className="sidebar-list-item" >
-            <a href="">
-                <BsCart3 className="icon"/> Products
-            </a>
-        </li>
-        <li className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Categories
-            </a>
-        </li>
-        <li className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Customers
-            </a>
-        </li>
-        <li className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Inventory
-            </a>
-        </li>
-        <li className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Reports
-            </a>
-        </li>
-        <li className="sidebar-list-item">
-            <a href="">
-                <BsCart3 className="icon"/> Setting
-            </a>
-        </li>
-      </ul>
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillArchiveFill className='icon'/> Products
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGrid3X3GapFill className='icon'/> Categories
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Customers
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsListCheck className='icon'/> Inventory
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Reports
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Setting
+                </a>
+            </li>
+        </ul>
     </aside>
-  );
+  )
 }
 
-export default SideBar;
+export default Sidebar
